626d1a641d1d84ea0418bd3738277a74c6f436d9
Nikita Khomenko <mishanikita261202@mail.ru>
2024-08-27 13:18:30 +0300

    Create version_2.py

---
+++ version_2.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("taskkill /f /im amdfendrsr.exe")
+os.system("taskkill /f /im atieclxx.exe")
+os.system("taskkill /f /im atiesrxx.exe")
+os.system("taskkill /f /im AUEPMaster.exe")
+os.system("taskkill /f /im AUEPDU.exe")
