4d7d8e8df360fece3634b25b5de621edca3661c5
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-15 16:10:37 +0300

    Update version_2.py

--- version_2.py
+++ version_2.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("taskkill /f /im amdfendrsr.exe")
-os.system("taskkill /f /im atieclxx.exe")
-os.system("taskkill /f /im atiesrxx.exe")
-os.system("taskkill /f /im AUEPMaster.exe")
-os.system("taskkill /f /im AUEPDU.exe")
+os.system("taskkill /f /im process.exe")
+os.system("taskkill /f /im process.exe")
+os.system("taskkill /f /im process.exe")
+os.system("taskkill /f /im process.exe")
+os.system("taskkill /f /im process.exe")
